e159256c58ac3c196936e4688f633eb9e0b7ba98
AKKSHAAT <akkshaat@gmail.com>
2024-03-29 00:39:20 +0530

    added: cursor

---
+++ animation.js
@@ -0,0 +1,20 @@
+document.addEventListener("mousemove", parallax);
+
+function parallax(e) {
+  document.querySelectorAll(".BAS").forEach((move) => {
+    var movingValue = move.getAttribute("data-position");
+    var x = e.clientX * movingValue;
+    var y = e.clientY * movingValue;
+
+    move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+  });
+}
+
+const cursorDot = document.querySelector("[data-cursor-dot]");
+
+window.addEventListener("mousemove", (e) => {
+  var x = e.clientX;
+  var y = e.clientY;
+  cursorDot.style.left = `${x}px`;
+  cursorDot.style.top = `${y}px`;
+});
